3aa6826ffcee438883988a031852d357659c5a4f
Rob UCSC <robucsc@gmail.com>
2020-06-25 14:28:42 -0700

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+// console.log("hello world");
+
+// create game configuration object
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+};
+
+// create main game object
+let game = new Phaser.Game(config);
