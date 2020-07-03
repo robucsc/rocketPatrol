7785c39598c26970a4ab1508a332ea0360a4417d
Rob UCSC <robucsc@gmail.com>
2020-07-02 18:04:11 -0700

    Updated Endgame Menu code

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -72,7 +72,7 @@ class Play extends Phaser.Scene{
         scoreConfig.fixedWidth = 0;
         this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
             this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
-            this.add.text(game.config.width/2, game.config.height/2 + 64, '(F)ire to Restart', scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + 64, '(F)ire to Restart or ← for Menu', scoreConfig).setOrigin(0.5);
             this.gameOver = true;
         }, null, this);
     }
@@ -80,6 +80,9 @@ class Play extends Phaser.Scene{
     update(){ // ideally every frame
         // check key input for restart, keyUP for one handed play
         if (this.gameOver && (Phaser.Input.Keyboard.JustDown(keyF) || Phaser.Input.Keyboard.JustDown(keyUP))){
+            this.scene.restart(this.p1Score);
+        }
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)){
             this.scene.start("menuScene");
         }
 
